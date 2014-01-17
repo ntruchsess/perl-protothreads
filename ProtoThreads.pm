11f758e0e976df40c902c97bf6905b6cc7601e5c
ntruchsess <norbert.truchsess@t-online.de>
2014-01-17 17:59:59 +0100

    add copyright notice

--- ProtoThreads.pm
+++ ProtoThreads.pm
@@ -1,3 +1,18 @@
+# Perl Protothreads
+# 
+# a lightwight pseudo-threading framework for perl that is
+# heavily inspired by Adam Dunkels protothreads for the c-language
+# 
+# LICENSE AND COPYRIGHT
+#
+# Copyright (C) 2014 ntruchsess (norbert.truchsess@t-online.de)
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of either: the GNU General Public License as published
+# by the Free Software Foundation; or the Artistic License.
+#
+# See http://dev.perl.org/licenses/ for more information.
+#
 #PT_THREAD(sub)
 #Declare a protothread
 #
