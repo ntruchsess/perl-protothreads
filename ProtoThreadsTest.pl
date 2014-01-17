d40c2e0f9358ea2cd8469e09d310933f2a9a6579
ntruchsess <norbert.truchsess@t-online.de>
2014-01-17 18:03:50 +0100

    add copyright notice to sample

--- ProtoThreadsTest.pl
+++ ProtoThreadsTest.pl
@@ -1,4 +1,56 @@
-package ProtoThreads;
+# Perl Protothreads
+# 
+# a lightwight pseudo-threading framework for perl that is
+# heavily inspired by Adam Dunkels protothreads for the c-language
+# 
+# LICENSE AND COPYRIGHT
+#
+# Copyright (C) 2014 ntruchsess (norbert.truchsess@t-online.de)
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of either: the GNU General Public License as published
+# by the Free Software Foundation; or the Artistic License.
+#
+# See http://dev.perl.org/licenses/ for more information.
+#
+#PT_THREAD(sub)
+#Declare a protothread
+#
+#PT_INIT(thread)
+#Initialize a thread
+#
+#PT_BEGIN(thread);
+#Declare the start of a protothread inside the sub implementing the protothread.
+#
+#PT_WAIT_UNTIL(condition);
+#Block and wait until condition is true.
+#
+#PT_WAIT_WHILE(condition);
+#Block and wait while condition is true.
+#
+#PT_WAIT_THREAD(thread);
+#Block and wait until another protothread completes.
+#
+#PT_SPAWN(thread);
+#Spawn a child protothread and wait until it exits.
+#
+#PT_RESTART;
+#Restart the protothread.
+#
+#PT_EXIT;
+#Exit the protothread.
+#
+#PT_END;
+#Declare the end of a protothread.
+#
+#PT_SCHEDULE(protothread);
+#Schedule a protothread.
+#
+#PT_YIELD;
+#Yield from the current protothread.
+#
+#PT_YIELD_UNTIL(condition);
+#Yield from the current protothread until the condition is true.
 
 use strict;
 use warnings;
